f3edb79b0400ca4e236806762e13411751d217b1
uxdibyajeet <ux.dibyajeet@gmail.com>
2026-06-03 17:15:01 +0000

    modal added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // dated 03.06.2026
 
 //stores all the books
-const myLibrary = [];
+let myLibrary = [];
 
 //book constructor
 const Book = function (name, author, pages, status) {
@@ -9,7 +9,6 @@ const Book = function (name, author, pages, status) {
     throw Error("forgot to add 'new' keyword");
   }
   this.status = status;
-
   this.id = crypto.randomUUID();
   this.name = name;
   this.author = author;
@@ -18,42 +17,72 @@ const Book = function (name, author, pages, status) {
 
 // take params create new book and adds to library
 function addBookToLibrary(name, author, pages, status) {
+  const addNew = document.querySelector("#addNew");
+  const modal = document.querySelector(".modal");
+  const buttons = document.querySelectorAll(".btn");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.textContent === "Add New Book") {
+        if (!modal.classList.contains("hidden")) {
+          modal.classList.add("hidden");
+        } else {
+          modal.classList.remove("hidden");
+        }
+      } else if (button.textContent === "Cancel") {
+        modal.classList.add("hidden");
+      } else {
+        addBookToLibrary();
+        console.log(myLibrary);
+      }
+    });
+  });
+
   const newBook = new Book(name, author, pages, status);
-  console.log(newBook);
   myLibrary.push(newBook);
 }
 
 const sample = {
-  name: "Book Name",
-  author: "Author Name",
-  pages: 89,
+  name: "(Sample)1984",
+  author: "George Orwell",
+  pages: 370,
   status: true,
 };
 addBookToLibrary(sample.name, sample.author, sample.pages, sample.status);
-addBookToLibrary(sample.name, sample.author, sample.pages, sample.status);
-addBookToLibrary(sample.name, sample.author, sample.pages, sample.status);
 
 //Table creator
-
 function generateTable() {
   const dataTable = document.querySelector("#table-body");
   dataTable.innerHTML = ""; //clear the table
   myLibrary.forEach((book) => {
-    const row = document.createElement("tr");
+    const row = document.createElement("tr"); //creates row for each item
     row.classList.add("row");
-    Object.values(book).forEach((val) => {
+    //data fields are generated from here
+    Object.keys(book).forEach((val) => {
       const cell = document.createElement("td");
+      if (val === "id") return;
+      else if (val === "status") {
+        const checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.checked = book[val];
+        // Check / uncheck event listener
+        checkBox.addEventListener("click", (e) => {
+          book.status = e.target.checked;
+          console.log(book.status);
+        });
+        cell.appendChild(checkBox);
+      } else {
+        cell.textContent = book[val];
+      }
       cell.classList.add("cell");
-      cell.textContent = val;
       row.appendChild(cell);
     });
     const actionCell = document.createElement("td");
-    const deleteBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button"); // delete action
     deleteBtn.textContent = "Delete";
 
     //event listner
     deleteBtn.addEventListener("click", () => {
-      deleteRow(book.id);
+      deleteBook(book.id);
     });
 
     actionCell.appendChild(deleteBtn);
@@ -64,9 +93,14 @@ function generateTable() {
 
 console.log(myLibrary);
 
-function deleteRow(itemId) {
-  myLibrary.splice(itemId, 1);
+// Delete book via ID
+function deleteBook(itemId) {
+  myLibrary = myLibrary.filter((book) => book.id !== itemId);
+  console.log(`Just Deleted item with ID: ${itemId}`);
   generateTable();
 }
+
+// add new Book
+function addBook() {}
 
 generateTable();
